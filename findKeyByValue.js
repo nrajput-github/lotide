cbde6d142924d2b2727bb4c976bfc52b827c43ab
Navya Rajput <navyasrajput@gmail.com>
2021-09-15 23:58:51 -0400

    Implement findKeyByValue

---
+++ findKeyByValue.js
@@ -0,0 +1,25 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+const findKeyByValue = function(myObject, value) {
+  for (let element in myObject) {
+    if (myObject[element] === value) {
+      return element;
+    }
+  }
+  return;
+
+};
+
+const bestTVShowsByGenre = {
+  "sci_fi": "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
