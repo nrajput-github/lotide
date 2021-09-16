98cb42395775578f49da6667d2c02f9d3f89a96c
Navya Rajput <navyasrajput@gmail.com>
2021-09-15 23:11:18 -0400

    Implement countLetters

---
+++ countLetters.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+const countLetters = function(myString) {
+  let myObject = {};
+  for (let element of myString) {
+    if (element !== ' ') {
+      if (myObject[element]) {
+        myObject[element]++;
+      } else {
+        myObject[element] = 1;
+      }
+    }
+  }
+
+  return myObject;
+
+};
+
+const result = countLetters("lighthouse in the house");
+assertEqual(result["l"], 1);
